1bbb552d829bdbc2bb199b1895335d7c4f92ab43
stormouse <stormouse@126.com>
2018-01-31 17:17:13 -0800

    Add Clannad Character
    added a clannad character and supplimentary materials and shaders

---
+++ Assets/Scripts/CapsulePlayerControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class CapsulePlayerControl : MonoBehaviour {
+
+    public float moveSpeed = 2.0f;
+
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Move(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        Vector3 movement = new Vector3(h, 0, v) * moveSpeed;
+        if (movement.magnitude > moveSpeed)
+            movement = movement.normalized * moveSpeed;
+        if (movement.magnitude > 0.5f)
+        {
+            if (_animator) _animator.SetBool("isWalking", true);
+            GetComponent<Rigidbody>().MoveRotation(Quaternion.LookRotation(movement));
+        }
+        else
+        {
+            if (_animator) _animator.SetBool("isWalking", false);
+        }
+        transform.Translate(movement * Time.deltaTime, Space.World);
+    }
+
+}
