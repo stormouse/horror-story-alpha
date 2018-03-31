636c8b760860142721135c0ee012ebc57c7bbc8a
stormouse <stormouse@126.com>
2018-03-31 16:48:28 -0700

    test level

---
+++ Assets/Lobby/Scripts/networklobby/TeamTag.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamTag : MonoBehaviour {
+
+    [SerializeField]
+    public GameEnum.TeamType team;
+}
