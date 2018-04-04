b7d21f9c79e4239ce68a9b3e076de36b46bda062
Miao <zhewenqq@live.com>
2018-04-03 19:06:41 -0700

    trap fix. new wolf

--- Assets/Prototype/Scripts/TrapControl.cs
+++ Assets/Prototype/Scripts/TrapControl.cs
@@ -5,18 +5,36 @@ using UnityEngine.Networking;
 public class TrapControl : NetworkBehaviour {
 
 	public float trapStunTime = 3.0f;
+    private bool triggered = false;
 
-	void OnTriggerEnter(Collider other) {
-		if (!isServer) {
+    private void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+		if (!isServer || triggered) {
 			return;
 		}
 		var otherCharacter = other.GetComponent<NetworkCharacter>();
 		if (otherCharacter && otherCharacter.Team == GameEnum.TeamType.Hunter) {
+            triggered = true;
 			var args = new StunArgument ();
 			args.time = trapStunTime;
-			otherCharacter.Perform ("Stun", other.gameObject, args);
-			NetworkServer.Destroy (gameObject);
+            otherCharacter.Perform("StopMovement", other.gameObject, null);
+            otherCharacter.Perform ("Stun", other.gameObject, args);
+            
+
+            GetComponent<Animator> ().SetBool("close", true);
+
+
+            Invoke("Byebye", trapStunTime);
+            //NetworkServer.Destroy (gameObject);
 		}
 
 	}
+
+    void Byebye() {
+        NetworkServer.Destroy(gameObject);
+    }
 }
