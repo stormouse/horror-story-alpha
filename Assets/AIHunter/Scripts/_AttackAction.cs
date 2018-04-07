39ff1240086915343674109c34ccc72b7524d6bc
usernickname <luxin0311@live.com>
2018-04-07 16:12:49 -0700

    hunter update

--- Assets/AIHunter/Scripts/_AttackAction.cs
+++ Assets/AIHunter/Scripts/_AttackAction.cs
@@ -5,19 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "AIHunter/Actions/_AttackAction")]
 public class _AttackAction : _Action {
 
-	public _State PatrolState;
-
 	public override void Act (_HunterStateController controller)
 	{
 		Attack (controller);
 	}
 
 	public void Attack(_HunterStateController controller){
-		//check target legal
-		if (null == controller.chaseTarget [0] || controller.chaseTarget [0].CurrentState == CharacterState.Dead) {
-			controller.TransitionToState (PatrolState);
-			return;
-		}
 		//attack kill here
 		controller.character.Perform ("Attack", controller.gameObject, null);
 		controller.chaseTarget [0].Perform ("Die", controller.chaseTarget[0].gameObject, null);
--- Assets/AIHunter/Scripts/_ChaseChaserAction.cs
+++ Assets/AIHunter/Scripts/_ChaseChaserAction.cs
@@ -5,19 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "AIHunter/Actions/_ChaseChaserAction")]
 public class _ChaseChaserAction : _Action {
 
-	public _State PatrolState;
-
 	public override void Act (_HunterStateController controller)
 	{
 		chase (controller);
 	}
 
 	public void chase(_HunterStateController controller){
-		//check target legal
-		if (null == controller.chaseTarget [0] || controller.chaseTarget [0].CurrentState == CharacterState.Dead) {
-			controller.TransitionToState (PatrolState);
-			return;
-		}
 		if (controller.character.CurrentState == CharacterState.Normal) {
 			controller.navMeshAgent.destination = controller.chaseTarget [0].transform.position;
 			controller.navMeshAgent.isStopped = false;
