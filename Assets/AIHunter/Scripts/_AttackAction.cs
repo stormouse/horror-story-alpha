4de718547eaefa5b65d9416b58f762ed2938d85e
usernickname <luxin0311@live.com>
2018-04-07 15:50:55 -0700

    wolf update

--- Assets/AIHunter/Scripts/_AttackAction.cs
+++ Assets/AIHunter/Scripts/_AttackAction.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "AIHunter/Actions/_AttackAction")]
 public class _AttackAction : _Action {
 
+	public _State PatrolState;
+
 	public override void Act (_HunterStateController controller)
 	{
 		Attack (controller);
 	}
 
 	public void Attack(_HunterStateController controller){
+		//check target legal
+		if (null == controller.chaseTarget [0] || controller.chaseTarget [0].CurrentState == CharacterState.Dead) {
+			controller.TransitionToState (PatrolState);
+			return;
+		}
 		//attack kill here
 		controller.character.Perform ("Attack", controller.gameObject, null);
 		controller.chaseTarget [0].Perform ("Die", controller.chaseTarget[0].gameObject, null);
+		controller.chaseTarget.Remove (controller.chaseTarget [0]);
 	}
 }
--- Assets/AIHunter/Scripts/_HookAction.cs
+++ Assets/AIHunter/Scripts/_HookAction.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "AIHunter/Actions/_HookAction")]
 public class _HookAction : _Action {
 
+	public _State PatrolState;
+
 	public override void Act (_HunterStateController controller)
 	{
-		lunchHook (controller);
+			lunchHook (controller);
 	}
 
 	public void lunchHook(_HunterStateController controller){
-
+		//check target legal
+		if (null == controller.chaseTarget [0] || controller.chaseTarget [0].CurrentState == CharacterState.Dead) {
+			controller.TransitionToState (PatrolState);
+			return;
+		}
 		//lunch hook here
 		DirectionArgument dir = new DirectionArgument();
 		dir.direction = controller.chaseTarget [0].transform.position - controller.transform.position;
