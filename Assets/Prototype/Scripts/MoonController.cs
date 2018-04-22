9c7cbb85d99e061fac2fed2f6ca5eeec2acbd352
Miao <zhewenqq@live.com>
2018-04-22 15:32:23 -0700

    Moving Star and Spectator Spwan point

---
+++ Assets/Prototype/Scripts/MoonController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonController : MonoBehaviour {
+
+    public float RotateSpeed = .3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+	void FixedUpdate () {
+        transform.Rotate(new Vector3(1f, 1f, 0f), RotateSpeed*Mathf.Deg2Rad);
+	}
+}
